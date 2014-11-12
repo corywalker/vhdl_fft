3c0ca1f4d697ba4733ca668a8867fc4d5e744b46
cmwslw <cwalker32@gmail.com>
2014-11-12 01:01:47 -0500

    Arduino sketch for oscilloscope.

---
+++ arduino/osc/osc.ino
@@ -0,0 +1,42 @@
+int startPin = 2;
+int busyPin = 3;
+int clockPin = 13;
+int dataPin = 12;
+
+void setup() {
+  pinMode(startPin, OUTPUT);
+  pinMode(busyPin, INPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, INPUT);
+  // initialize serial:
+  Serial.begin(115200);
+}
+
+int iter = 0;
+
+void loop() {
+  
+  // Throw out first bit
+  digitalWrite(clockPin, LOW);
+  digitalWrite(startPin, HIGH);
+  digitalWrite(startPin, LOW);
+  
+  delayMicroseconds(5);
+  
+  while(digitalRead(busyPin)) {}
+  
+  delayMicroseconds(5);
+  
+  for (int j = 0; j < 4; j++) {
+    // Read the 16 bit value
+    unsigned int val = 0;
+    for (int i = 0; i < 16; i++) {
+      digitalWrite(clockPin, HIGH);
+      val += digitalRead(dataPin) << (15-i);
+      digitalWrite(clockPin, LOW);
+    }
+    Serial.println(val);
+  }
+  
+  delay(1);
+}
