04f8e8d195dff66b8aaa5972fd75a779d74d1396
cmwslw <cwalker32@gmail.com>
2014-11-21 13:26:22 -0500

    Created vhdlfft Python module.

--- arduino/tktest.py
+++ arduino/tktest.py
@@ -1,15 +1,5 @@
 from Tkinter import *
-import serial
-import struct
-import itertools
-
-def grouper(n, iterable):
-    it = iter(iterable)
-    while True:
-       chunk = tuple(itertools.islice(it, n))
-       if not chunk:
-           return
-       yield chunk
+from vhdlfft import VHDLFFT
 
 root = Tk()
 root.title("move-and-erase")
@@ -18,25 +8,23 @@ ch = 256 # canvas height
 
 chart_1 = Canvas(root, width=cw, height=ch, background="white")
 chart_1.grid(row=0, column=0)
+cycle_period = 1000 # time between new positions of the ball
+                             # (milliseconds).
 cycle_period = 50 # time between new positions of the ball
                              # (milliseconds).
 
 SIZE = 512
-ser = serial.Serial("/dev/tty.usbmodem14211", 500000, timeout=0.05)
+vf = VHDLFFT("/dev/tty.usbmodem14211", 512, 9)
 
 for i in range(1,500):   # end the program after 500 position shifts.
-    ser.write("S")
-    data = ser.read(SIZE*2)
+    data = vf.read()
     print len(data)
-    if len(data) == SIZE*2:
-        data = struct.unpack('BB'*SIZE, data)
-
-        for x, sample in enumerate(grouper(2, data)):
-            real, im = sample
-            chart_1.create_line(x, 0, x, real)
+    if len(data) == SIZE:
+        for x, mag in enumerate(data):
+            chart_1.create_line(x, 0, x, mag)
 
         chart_1.update() # This refreshes the drawing on the canvas.
-        chart_1.after(cycle_period) # This makes execution pause for 200
+        #chart_1.after(cycle_period) # This makes execution pause for 200
         chart_1.delete(ALL) # This erases everything on the canvas.
 
 root.mainloop()
